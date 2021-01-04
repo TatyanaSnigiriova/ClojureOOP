b542a28768569b0bf077c728dab68c3c9976558f
TatyanaSnigiriova <tatyanakolesnik2016@yandex.ru>
2021-01-04 19:51:44 +0700

    It's some debaging of def-doc-type macro. Creating tasks to do

--- src/nsu1/command_query.clj
+++ src/nsu1/command_query.clj
@@ -2,30 +2,37 @@
 
 (use 'clojure.set)
 
-(def doc-hierarchy (ref {}))
-;аналог Object, не может быть определен штатно
+(def doc-hierarchy
+  "Tree of object types ready for instantiation"
+  (ref {})
+)
+
+; Аналог Object, не может быть определен штатно
 (dosync
   (alter doc-hierarchy
     (fn [h]
       (assoc h
         :Document {
-          ::type :Document,
+          ::type :Document                                  ; ToDo Так ли он нам нужен?
+                                                            ; Можно же условится, что ключ сам по себе будет являться типом
           ::super nil
           ::fields {}
           ::init nil
         }))))
 
 (defmacro def-doc-type [name supers fields & sections]
+  "Adding information about the new class to the hierarchy if its ancestor exists
+  (otherwise, the base class must be specified)"
   (let [
       sections (apply merge (map eval sections))
       init (get sections :init)
     ]
     `(let [
-      super# (if (empty? '~supers) (list :Document) '~supers)
-      fields# (set '~fields)
-    ]
-    ; Добавляем запись о новом классе в иерархию
-    (dosync
+         super# (if (empty? '~supers) (list :Document) '~supers)
+         fields# (set '~fields)                              ; Это странно, но без ' в отладчике поля всегда пусты
+      ]
+
+      (dosync
       (assert (not (contains? @doc-hierarchy ~name))
         (format "Forbidden new class name %s." ~name)
       )                                                     ; Обязательное наследование от базового класса
@@ -33,12 +40,16 @@
         doc-hierarchy
         (fn [h#]
           (assoc
+            ;; private
             h# ~name {
-              ::type ~name
+              ::type '~name
               ::super super#
               ::fields fields#
               ::init '~init
             }
+               ; По сути, возможость отладки появляется только после успешного добавления в иерархию или при помощи macroexpand
+               ; Не совсем ясно, почему '~ без ' вызывает ошибку - значения вычисляются?
+               ; Разобраться - ToDo
           )))))))
 
 (defn init [field value & map]
@@ -46,9 +57,14 @@
   {:init (apply hash-map field value map)}
 )
 
-; Вопросы: Инит работает довольно слепо:
+; ToDo init работает довольно слепо:
 ; Что если инициализируемого поля просто не будет в списке полей для текущего типа?
 ; Если мы даём возможность задавать начальное значение для родительских полей, опять же, как мы должны проверять их наличие?
 
-; Сейчас наследование как таковое не реализовано, тк требуется считывать дерево иерархии от родителя к наследникам, чтобы получить
-; Весь набор полей и их начальных значений, которые, опять же, могут быть переопределены в наследниках
+; ToDo Сейчас наследование как таковое не реализовано, т.к. требуется считывать дерево иерархии от родителя к наследникам,
+; чтобы получить весь набор полей и их начальных значений, которые, опять же, могут быть переопределены в наследниках.
+
+; Return only TypeName, not structure for superClass
+(defn get-super-class-type [class]
+  (get (get @doc-hierarchy class) ::super)
+)
